5b63fbaecfc26d5fb588294446aaeb2a94c8ed18
Mohamed ELhariry <119127084+mohamedelhariry00@users.noreply.github.com>
2024-12-27 15:43:35 +0200

    Procedure_ActiveMemberReports.sql

--- Procedure_ActiveMemberReports.sql
+++ Procedure_ActiveMemberReports.sql
@@ -1,32 +1,22 @@
 
-
-
-CREATE OR REPLACE PROCEDURE generate_reports AS
+CREATE OR REPLACE PROCEDURE active_members_reports AS
 BEGIN
     
-    -- تقرير الأعضاء الأكثر نشاطًا
+    -- Most Active Members
     DBMS_OUTPUT.PUT_LINE('Most Active Members:');
     FOR member_rec IN (
-        SELECT m.name, COUNT(l.id) AS loan_count
-        FROM members m
-        JOIN loans l ON m.id = l.member_id
-        GROUP BY m.name
+    SELECT * FROM(
+        SELECT members.name, COUNT(loans.id) AS loan_count
+        FROM members 
+        JOIN loans  ON members.id = loans.member_id
+        GROUP BY members.name
         ORDER BY loan_count DESC
-        FETCH FIRST 10 ROWS ONLY
+     -- FETCH FIRST 10 ROWS ONLY
+        )
+        WHERE ROWNUM <= 10
     ) LOOP
         DBMS_OUTPUT.PUT_LINE(member_rec.name || ': ' || member_rec.loan_count || ' loans');
     END LOOP;
 
-    -- تقرير الغرامات المحصلة خلال فترة معينة
-    DBMS_OUTPUT.PUT_LINE('Fines Collected:');
-    FOR fine_rec IN (
-        SELECT SUM(l.fine_amount) AS total_fines, TO_CHAR(l.return_date, 'YYYY-MM') AS month
-        FROM loans l
-        WHERE l.fine_amount > 0
-        GROUP BY TO_CHAR(l.return_date, 'YYYY-MM')
-        ORDER BY month
-    ) LOOP
-        DBMS_OUTPUT.PUT_LINE('Month: ' || fine_rec.month || ', Total Fines: $' || fine_rec.total_fines);
-    END LOOP;
 END;
 /
