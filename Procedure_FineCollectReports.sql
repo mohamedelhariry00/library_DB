2ee1d7bc9850f2a0101538936ea4c383011ac9eb
Mohamed ELhariry <119127084+mohamedelhariry00@users.noreply.github.com>
2024-12-27 15:51:03 +0200

    Procedure_FineCollectReports.sql

--- Procedure_FineCollectReports.sql
+++ Procedure_FineCollectReports.sql
@@ -1,5 +1,4 @@
-
-
+--
 
 CREATE OR REPLACE PROCEDURE fine_collect_reports AS
 BEGIN
@@ -7,10 +6,10 @@ BEGIN
     -- Fines Collection
     DBMS_OUTPUT.PUT_LINE('Fines Collected:');
     FOR fine_rec IN (
-        SELECT SUM(l.fine_amount) AS total_fines, TO_CHAR(l.return_date, 'YYYY-MM') AS month
-        FROM loans l
-        WHERE l.fine_amount > 0
-        GROUP BY TO_CHAR(l.return_date, 'YYYY-MM')
+        SELECT SUM(loans.fine_amount) AS total_fines, TO_CHAR(loans.return_date, 'YYYY-MM') AS month
+        FROM loans 
+        WHERE loans.fine_amount > 0
+        GROUP BY TO_CHAR(loans.return_date, 'YYYY-MM')
         ORDER BY month
     ) LOOP
         DBMS_OUTPUT.PUT_LINE('Month: ' || fine_rec.month || ', Total Fines: $' || fine_rec.total_fines);
